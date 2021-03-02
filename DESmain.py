1cb4756fef1afd53fc928a0d219afef3535e2700
Ingolf Magnus <ingolfmagnus@gmail.com>
2021-03-02 19:27:57 +0100

    Encrypt/Decrypt works on 64bit block

--- DESmain.py
+++ DESmain.py
@@ -1,6 +1,10 @@
 def main():
+    print("encrypting ", hex(0x0123456789ABCDEF), " ...")
     C = encrypt(0x0123456789ABCDEF, 0x133457799BBCDFF1)
-    print("encrypted to: ", C)
+    print("encrypted to: ", hex(C))
+    print("decrypting ", hex(C), " ...")
+    D = decrypt(C, 0x133457799BBCDFF1)
+    print("decrypted to: ", hex(D))
     return
 
 
@@ -41,28 +45,125 @@ E = [32, 1, 2, 3, 4, 5,
      24, 25, 26, 27, 28, 29,
      28, 29, 30, 31, 32, 1]
 
+S = [
+    # S1
+    [
+        [14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7],
+        [0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8],
+        [4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0],
+        [15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13]
+    ],
 
-def f(v32, k48):
-    e48 = permute(v32, E)
-    ke48 = e48 ^ k48
+    # S2
+    [
+        [15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10],
+        [3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5],
+        [0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15],
+        [13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9],
+    ],
 
-    pass
+    # S3
+    [
+        [10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8],
+        [13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1],
+        [13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7],
+        [1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12],
+    ],
 
+    # S4
+    [
+        [7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15],
+        [13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9],
+        [10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4],
+        [3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14],
+    ],
 
-def encrypt(M, K):
+    # S5
+    [
+        [2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9],
+        [14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6],
+        [4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14],
+        [11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3],
+    ],
+
+    # S6
+    [
+        [12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11],
+        [10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8],
+        [9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6],
+        [4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13],
+    ],
+
+    # S7
+    [
+        [4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1],
+        [13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6],
+        [1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2],
+        [6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12],
+    ],
+
+    # S8
+    [
+        [13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7],
+        [1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2],
+        [7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8],
+        [2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11],
+    ]
+]
+
+P = [16, 7, 20, 21,
+     29, 12, 28, 17,
+     1, 15, 23, 26,
+     5, 18, 31, 10,
+     2, 8, 24, 14,
+     32, 27, 3, 9,
+     19, 13, 30, 6,
+     22, 11, 4, 25]
+
+IPR = [40, 8, 48, 16, 56, 24, 64, 32,
+       39, 7, 47, 15, 55, 23, 63, 31,
+       38, 6, 46, 14, 54, 22, 62, 30,
+       37, 5, 45, 13, 53, 21, 61, 29,
+       36, 4, 44, 12, 52, 20, 60, 28,
+       35, 3, 43, 11, 51, 19, 59, 27,
+       34, 2, 42, 10, 50, 18, 58, 26,
+       33, 1, 41, 9, 49, 17, 57, 25
+       ]
+
+def decrypt(M, K):
+    return encrypt(M, K, reverse=True)
+
+def encrypt(M, K, reverse=False):
     subkeys = generateroundkeys(K)
 
-    MIP = permute(M, IP)
+    MIP = permute(M, IP, wordsize=64)
 
     L, R = getlr32(MIP)
-
+    if (reverse):
+        subkeys.reverse()
     for SK in subkeys:
         Lprev = L
         Rprev = R
         L = Rprev
         R = Lprev ^ f(Rprev, SK)
+    RL = (R << 32) + L
+    C = permute(RL, IPR, wordsize=64)
 
-    return 0
+    return C
+
+
+def f(v32, k48):
+    v48 = permute(v32, E, wordsize=32)
+    kxv48 = v48 ^ k48
+    result32 = 0
+    for b in range(8):
+        block6 = getbits(kxv48, b * 6 + 1, 6, wordsize=48)
+        srow = (getbit(block6, 1, wordsize=6) << 1) + getbit(block6, 6, wordsize=6)
+        scol = getbits(block6, 2, 4, wordsize=6)
+        block4 = S[b][srow][scol]
+        result32 = (result32 << 4) + block4
+    result32 = permute(result32, P, wordsize=32)
+    return result32
 
 
 def generateroundkeys(K):
@@ -70,7 +171,7 @@ def generateroundkeys(K):
     CD = 0
     shifts = [1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1]
 
-    Kplus = permute(K, PC1)
+    Kplus = permute(K, PC1, wordsize=64)
 
     C, D = getlr28(Kplus)
 
@@ -79,16 +180,16 @@ def generateroundkeys(K):
             C = ((C << 1) + getbit(C, 1, wordsize=28)) & 0xfffffff
             D = ((D << 1) + getbit(D, 1, wordsize=28)) & 0xfffffff
         CD = C * 2 ** 28 + D
-        keys.append(permute(CD, PC2))
+        keys.append(permute(CD, PC2, wordsize=56))
     return keys
 
 
-def permute(V, permutation):
+def permute(V, permutation, wordsize):
     P = 0
     for i in permutation:
         P <<= 1
-        P += getbit(V, 1)
-    return V
+        P += getbit(V, i, wordsize)
+    return P
 
 
 def splithalves(bits, halfsize):
@@ -111,6 +212,10 @@ def getlr28(bits):
 
 def getbit(value, bit, wordsize=64):
     return value >> (wordsize - bit) & 1
+
+
+def getbits(value, startbit, numbits, wordsize):
+    return value >> (wordsize - startbit - numbits + 1) & ((1 << numbits) - 1)
 
 
 def bin32(value):
